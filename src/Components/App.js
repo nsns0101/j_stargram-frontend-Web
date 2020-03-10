eef38b7bfac4a81ac0d4d3d3eb4db650a426eb92
nsns0101 <nsns0101@naver.com>
2020-03-10 17:57:46 +0900

    ex - isLoggedIn Query

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,31 @@
 import React from "react";
 import GlobalStyles from "../Styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles />
-      <AppRouter isLoggedIn={false} />
-    </>
-  </ThemeProvider>
-);
+//apolloClient에서 isLoggedIn을 불러옴
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+  console.log(isLoggedIn);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <AppRouter isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  );
+};
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,9 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+//props는 부모 컴포넌트가 자식 컴포넌트에게 주는 값 (부모 자식 관계를 보고 싶으면 App.js를 참고)
 export default createGlobalStyle`
     ${reset}
     *{
         box-sizing:boder-box;
+    }
+    body {
+        background-color:${props => props.theme.bgColor};
+        color:${props => props.theme.blackColor};
+    }
+    a {
+        color:${props => props.theme.blueColor};
+        text-decoration:none;
     }
 `;
